44d86b2ffffe498fa8f15b55d48bd22168ca15c9
Pavel <jaredmanfarrell@gmail.com>
2022-01-19 09:40:53 +0300

    take photo from btgp.ru

--- test_python_org_search.py
+++ test_python_org_search.py
@@ -1,15 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+import webbrowser
 
 def main():
     driver = webdriver.Chrome()
     driver.get('https://btgp.ru/zameny-v-raspisanii.html')
     assert "Расписание занятий техникума и замены" in driver.title
     # btn = driver.find_element(By.CLASS_NAME, 'page-header')
-    f ='div[@class, "item column-1"]//last::*//span//a'
-    btn = driver.find_element(By.CSS_SELECTOR, f)
-    print(btn.get_attribute('href'))
+    csspath_to_last_link = '//div[contains(@class, "item column-1")]/p[last()]//a'
+    lastLink = driver.find_element(By.XPATH, csspath_to_last_link).get_attribute("href")
+    print(lastLink)
+    # webbrowser.get(using=None).open(lastLink)
+    # lastLink.click()
 
 
 if __name__ == '__main__':
